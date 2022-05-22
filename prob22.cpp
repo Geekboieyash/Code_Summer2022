bf7e02072a8769046a313820d0a91d3d08e003c7
Yash Mishra <89203577+Geekboieyash@users.noreply.github.com>
2022-05-22 19:25:46 +0530

    2 problems 22/05/21

--- prob22.cpp
+++ prob22.cpp
@@ -2,17 +2,35 @@
 using namespace std;
 #define _fast ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define ll long long int
-void solve()
-{
-    
-}
-int main()
-{
-    _fast;
-    int t; cin>>t;
-    while(t--)
-    {
-      solve();
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+
+/**
+ * solution: 
+ * class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int uni = 1, curr= nums[0];
+        for(int i =1 ; i<nums.size(); i++)
+        {
+            if(nums[i]>curr)
+            {
+                nums[uni] = nums[i];
+                uni++;
+                curr = nums[i];
+            }
+        }
+        return uni++;
     }
-    return 0;
-}
+};
+
+another solution using stl functions: 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        return nums.size();
+    }
+};
+*/
