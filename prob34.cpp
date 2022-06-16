10432cc661b2416ac460deea7aac134e5315f821
Yash Mishra <89203577+Geekboieyash@users.noreply.github.com>
2022-06-16 20:37:24 +0530

    16/06

--- prob34.cpp
+++ prob34.cpp
@@ -1,3 +1,78 @@
 //https://leetcode.com/problems/subarray-sum-equals-k/
 
+// contigious array sum 
 
+/**
+ * Input:
+    nums = [3,9,-2,4,1,-7]
+    k = 5
+
+
+Step1:
+    nums = [3,9,-2,4,1,-7]
+            ^
+    count = 0
+    sum = 3
+    map = [3:1]
+    k = 5
+
+    # 1 (i.e., runningSum-k, 3-5=-2) not found in map
+    # add the curr sum 3 in map
+
+Step2:
+    nums = [3,9,-2,4,1,-7]
+              ^
+    count = 0
+    sum = 12
+    map = [3:1, 12:1]
+
+    # 0 (12-5) not found in map
+    # add the curr sum 12 in map
+
+Step3:
+    nums = [3,9,-2,4,1,-7]
+                 ^
+    count = 0
+    sum = 10
+    map = [3:1, 12:1, 10:1]
+
+    # 7 (10-5) not found in map
+    # add the curr sum 10 in map
+
+Step4:
+    nums = [3,9,-2,4,1,-7]
+                   ^
+    count = 0
+    sum = 14
+    map = [3:1, 12:1, 10:1, 14:1]
+
+    # 4 (14-5) not found in map.
+    # add the curr sum 14 in map
+
+Step5:
+    nums = [3,9,-2,4,1,-7]
+                     ^
+    count = 1
+    sum = 15
+    map = [3:1, 12:1, 10:1, 14:1 , 15:1]
+
+    # 5 (15-5) found in map . Increment the count
+	#add cur sum 15 in map
+	
+Step 6:
+    nums = [3,9,-2,4,1,-7]
+                        ^
+    count = 2
+    sum = 8
+    map = [3:1, 12:1, 10:1, 14:1 , 15:1,8:1]
+
+    # 6 (8-5) found in map . Increment the count
+    #add cur sum 8 in map
+  
+*/
+
+
+// solution 
+
+ //similar questions : https://leetcode.com/problems/subarray-product-less-than-k/
+ //https://leetcode.com/problems/continuous-subarray-sum/
--- prob38.cpp
+++ prob38.cpp
@@ -10,15 +10,41 @@ void solve()
 {
   int a,b,c; cin>>a>>b>>c;
   // 2^(i-1) operation used at any one of three number at a time
-  
-    
+  // right shift operators will be used 
+  bool  good = false ;
+  for (int i = 0; i<40; i++)
+  {
+    if( a==b and b==c)
+    {
+      good = true; 
+      break; 
+    }
+    /*
+    int ct = ((a>>i)&1) + ((b>>i)&1) + ((c>>i)&1);
+			if (ct == 2) {
+				if (((a>>i)&1) == 0) a += 1LL<<i;
+				if (((b>>i)&1) == 0) b += 1LL<<i;
+				if (((c>>i)&1) == 0) c += 1LL<<i;
+			}
+			else if (ct == 1) {
+				if ((a>>i)&1) a += 1LL<<i;
+				if ((b>>i)&1) b += 1LL<<i;
+				if ((c>>i)&1) c += 1LL<<i;
+			}
+			else break;
+    */
+
+  }
+  if (good) cout<<"YES"<<"\n";
+  else cout<<"NO"<<"\n";
+
 }
 int main()
 {
     _fast;
 #ifndef ONLINE_JUDGE
-freopen("output.txt", "w", stdout);
-freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  freopen("input.txt", "r", stdin);
 #endif
     int t; cin>>t;
     while(t--)
