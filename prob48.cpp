a52fccac07e8d478c6b2ac56f6bccd81c05393c4
Yash Mishra <89203577+Geekboieyash@users.noreply.github.com>
2022-06-19 20:43:09 +0530

    19/06

---
+++ prob48.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _INVINCI ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long int
+#define vll vector<ll>
+const int N=1e5+11,mod=1e9+7;
+#define pb push_back
+void solve()
+{
+    int n; cin>>n;
+    
+}
+int main()
+{
+    _INVINCI;
+#ifndef ONLINE_JUDGE
+freopen("output.txt",, "w", stdout);
+freopen("input.txt", "r", stdin);
+#endif
+    int t; cin>>t;
+    while(t--)
+    {
+      solve();
+    }
+    return 0;
+}
