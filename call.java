f7834ad93f63ed8dbadcabd10b1f9d375593d852
Yash Mishra <89203577+Geekboieyash@users.noreply.github.com>
2022-08-27 20:05:35 +0530

    27-08

---
+++ call.java
@@ -0,0 +1,16 @@
+class call{
+	public static void main(String args[])
+	{
+		System.out.println("Call ME babe");
+		PickUpCall p1=new PickUpCall();
+		p1.show();
+	}
+}
+
+class PickUpCall
+{
+	public void show()
+	{
+		System.out.println("YO babe whatssup");
+	}
+}
---
+++ test.java
@@ -0,0 +1,7 @@
+public class Test
+{
+	public static void main(String[] args)
+	{
+		java.util.ArrayList l = new java.util.ArrayList(); 
+	}	
+}
