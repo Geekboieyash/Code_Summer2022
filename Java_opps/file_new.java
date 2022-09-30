5aa1a6ac52b145a70c07c82929efcc876585d5a0
Yash Mishra <89203577+Geekboieyash@users.noreply.github.com>
2022-09-30 11:18:47 +0530

    java file handling

---
+++ Java_opps/file_new.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;   
+import java.io.IOException;  
+import java.util.Scanner; 
+
+
+public class CreateFile {
+    static string path = "";
+    public static void main(string[] args)
+    {
+        try{
+           FileWriter newfile = new file
+        }
+    }
+}
